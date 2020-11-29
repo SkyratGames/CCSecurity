91a0fd3049f178c2488fbe70dad85b39d35f45d1
RoboSkyrat <ricky@skyrat.dev>
2020-11-29 11:59:04 -0600

    Initial Commit

---
+++ src/main/java/dev/skyrat/ccsecurity/CCSecurity.java
@@ -0,0 +1,10 @@
+package dev.skyrat.ccsecurity;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CCSecurity implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/skyrat/ccsecurity/client/CcsecurityClient.java
@@ -0,0 +1,13 @@
+package dev.skyrat.ccsecurity.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class CcsecurityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
